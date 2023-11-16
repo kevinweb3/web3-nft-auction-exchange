6dd7336c1197d947a25960529ee9d5a84a1e5436
kevinweb3 <kevinzhw@outlook.com>
2023-11-16 17:29:32 +0800

    commit

---
+++ build/precache-manifest.8307df3d73c0cb2d07506d16ba550e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855ad8ab04abbcb604cd8ecf653798ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd4dea71537baf7c1ceb",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "0b3a733e1b6eb9261b32",
+    "url": "/static/js/2.d0e4d20e.chunk.js"
+  },
+  {
+    "revision": "9d39fc3cceca5eb88ece1094a94fe229",
+    "url": "/static/js/2.d0e4d20e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4dea71537baf7c1ceb",
+    "url": "/static/js/main.050d3dc0.chunk.js"
+  },
+  {
+    "revision": "3dffe0e71321427f758f",
+    "url": "/static/js/runtime-main.ea45121c.js"
+  }
+]);
